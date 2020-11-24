447e4d5a33b7d9bc97cc8a1da470c521638decf3
Tungway1990 <68179274+Tungway1990@users.noreply.github.com>
2020-11-24 23:20:42 +0800

    Update restnet.py

--- restnet.py
+++ restnet.py
@@ -17,7 +17,7 @@ def Conv_block(X,X_shortcut,feature_map,stride,name):
             X_shortcut=X
     return X,X_shortcut
 
-def skip_connect_block(X,X_shortcut,feature_map,name):
+def Skip_connect_block(X,X_shortcut,feature_map,name):
     X_shortcut=keras.layers.ZeroPadding2D((1, 1),name='skip_connect_padding'+name)(X_shortcut)
     X_shortcut = keras.layers.Conv2D(feature_map, (3, 3), strides=(2,2), name='skip_connect_Conv'+name)(X_shortcut)
     X_shortcut = keras.layers.BatchNormalization(axis=3, name='skip_connect_bn'+name)(X_shortcut)
@@ -56,17 +56,17 @@ X,X_shortcut=Conv_block(X,X_shortcut,64,(1, 1),'2.2.')
 X,X_shortcut=Conv_block(X,X_shortcut,64,(1, 1),'2.3.')
 
 #Conv3
-X,X_shortcut=skip_connect_block(X,X_shortcut,128,'3.1.')
+X,X_shortcut=Skip_connect_block(X,X_shortcut,128,'3.1.')
 X,X_shortcut=Conv_block(X,X_shortcut,128,(1, 1),'3.2.')
 X,X_shortcut=Conv_block(X,X_shortcut,128,(1, 1),'3.3.')
 
 #Conv4
-X,X_shortcut=skip_connect_block(X,X_shortcut,256,'4.1.')
+X,X_shortcut=Skip_connect_block(X,X_shortcut,256,'4.1.')
 X,X_shortcut=Conv_block(X,X_shortcut,256,(1, 1),'4.2.')
 X,X_shortcut=Conv_block(X,X_shortcut,256,(1, 1),'4.3.')
 
 #Conv5
-X,X_shortcut=skip_connect_block(X,X_shortcut,512,'5.1.')
+X,X_shortcut=Skip_connect_block(X,X_shortcut,512,'5.1.')
 X,X_shortcut=Conv_block(X,X_shortcut,512,(1, 1),'5.2.')
 X,X_shortcut=Conv_block(X,X_shortcut,512,(1, 1),'5.3.')
 
